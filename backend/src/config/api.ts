13b7b4ab7eb938e89e1dbb848634477bfe9bcc07
avsnaelfrb <annabhaniy@gmail.com>
2026-01-13 14:59:49 +0800

    feat: api axios

--- backend/src/config/api.ts
+++ backend/src/config/api.ts
@@ -1,3 +1,15 @@
 import axios from "axios";
+import type { Response } from "express";
 
-const api_key  = process.env.TMDB_API_KEY
+const api_key = process.env.TMDB_API_KEY
+
+const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/authentication',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${api_key}` 
+    }
+}
+
+axios.request(options).then(res => console.log(res.data)).catch(error => console.error(error))
